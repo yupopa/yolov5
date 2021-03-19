08832017cf07fd34d466084bdec2d1e77e7c146f
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-19 23:23:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,30 +5,34 @@ import torch
 from PIL import Image
 
 
-
-
-
-
-
-model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
- 
-
-img1 = Image.open('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
-
-# Inference
-result = model(img1, size=640)  # includes NMS
-result.print()  
-result.save() 
-  
+class Predict:
+    def __init__(self, filename):
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
+        if self.img is not None:
+            self.display_output()
+            
             
     
- 
+    @staticmethod
+    def get_image_from_upload():
+        uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+        if uploaded_file is not None:
+            return PILImage.create((uploaded_file))
+        return None
+
+    def display_output(self):
+        st.image(self.img.to_thumb(500,500), caption='Uploaded Image')
+        # Inference
+        result = model(img1, size=640)  # includes NMS
+        result.print()  
+        result.save() 
+        st.image("results/uploaded_file")
+
+
+   
+if __name__=='__main__':
+    predictor = Predict(best.pt)
     
-st.image("results/BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg")
-
-
-
-
 
 
 
