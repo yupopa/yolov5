7b8fa9c393b2876095dfb0b176213cd76e6427e5
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-20 01:08:24 +0300

    Update nnewapp.py

--- nnewapp.py
+++ nnewapp.py
@@ -14,14 +14,26 @@ uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
 
 
 
-      
+
+class Predict:
+    def __init__(self, x):
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model= x)
+        self.display_output()
      
             
             
-result = model(uploaded_file, size=640)
-results.render()  # updates results.imgs with boxes and labels
-result.print()  
-result.save() 
-st.image("results/uploaded_file")
+  
+
+    def display_output(self):
+        # Inference
+        
+        result = model(uploaded_file, size=640)
+        results.render()  # updates results.imgs with boxes and labels
+        # includes NMS
+        result.print()  
+        result.save() 
+        st.image("results/uploaded_file")
         #st.image(results.render())
 
+if __name__=='__main__': 
+    predictor = Predict(x)
