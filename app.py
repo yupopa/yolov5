b211d120e41cce9fc3e06b7c6983881a261961a5
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-19 23:35:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,37 +3,26 @@ from urllib.request import urlretrieve
 import torch
 
 from PIL import Image
-filename = 'best.pt'
 
-class Predict:
-    def __init__(self, filename):
-        model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=filename)
-        if self.img is not None:
-            self.display_output()
+model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
+        
             
-            
-    
-    @staticmethod
-    def get_image_from_upload():
-        uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
-        if uploaded_file is not None:
-            return PILImage.create((uploaded_file))
-        return None
 
-    def display_output(self):
-        st.image(self.img.to_thumb(500,500), caption='Uploaded Image')
-        # Inference
-        result = model(img1, size=640)  # includes NMS
-        result.print()  
-        result.save() 
-        st.image("results/uploaded_file")
+uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+     
+PILImage.create((uploaded_file))
+
+
+
+st.image(self.img.to_thumb(500,500), caption='Uploaded Image')
+ 
+result = model(uploaded_file, size=640)  # includes NMS
+result.print()  
+result.save() 
+st.image("results/uploaded_file")
 
 
    
-
-if __name__=='__main__':
-    predictor = Predict(filename)
-
 
 
 
