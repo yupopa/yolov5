970b5eb328282045369685a4601079c6c8687ec7
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-20 02:44:42 +0300

    Update nnewapp.py

--- nnewapp.py
+++ nnewapp.py
@@ -27,6 +27,9 @@ class Predict:
     @staticmethod
     def get_image_from_upload():
         uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+          if uploaded_file is not None:
+            return PILImage.create((uploaded_file))
+        return None
         
         
         
@@ -39,7 +42,7 @@ class Predict:
             results.save()
 
        
-        for imageName in glob.glob('uploaded_file'): #assuming JPG
+        for imageName in glob.glob('/results/*.jpg'): 
             display(Image(filename=imageName))
             st.image(imageName)
  
