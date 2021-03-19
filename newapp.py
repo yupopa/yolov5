6dc6a7503d4c89bd5f371604bf02ac3428464bf0
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-20 02:05:35 +0300

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,1 +1,20 @@
-print("hello")
+import glob
+import torch
+from urllib.request import urlretrieve
+
+url = ("http://dl.dropboxusercontent.com/s/fkdy4rbf8g8wm2s/best.pt?raw=1")
+filename = "best.pt"
+urlretrieve(url,filename)
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=filename)
+
+from PIL import Image
+
+im_paths = glob.glob('/content/test/images/*.jpg')
+
+for i in range(len(im_paths)):
+  img = Image.open(im_paths[i])
+  results = model(img, size=160)  # includes NMS
+  results.print()  
+  results.save()
+  
