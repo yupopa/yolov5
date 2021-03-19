3ed7fc04b323016bf36b4e9410687331cb4dbc26
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-20 02:47:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,40 @@
-import streamlit as st
-from urllib.request import urlretrieve
-import torch
-
-from PIL import Image
 import glob
-
-
+import torch
+from urllib.request import urlretrieve
 from PIL import Image
 from IPython.display import Image, display
+import streamlit as st
+from fastai import *
+from fastai.vision.widgets import *
+from fastai.vision.all import *
 
 
 
 x = "best.pt"
-model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model= x)
-model = model.autoshape()
-uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
 
-im_paths = glob.glob('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
+im_paths = glob.glob('uploaded_file')
+
 
 
 
 class Predict:
-    def __init__(self, x):
+    def __init__(self, filename):
         model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model= x)
-        self.display_output()
-     
+        self.img = self.get_image_from_upload()
+        if self.img is not None:
+            self.display_output()
+ 
             
-            
-  
+    
+    @staticmethod
+    def get_image_from_upload():
+        uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+        if uploaded_file is not None:
+            return PILImage.create((uploaded_file))
+        return None
+        
+        
+        
 
     def display_output(self):
         for i in range(len(im_paths)):
@@ -35,10 +42,8 @@ class Predict:
             results = model(img, size=160)  # includes NMS
             results.print()  
             results.save()
+            st.image("/results/*.jpg")
+ 
 
-if __name__=='__main__': 
+if __name__=='__main__':
     predictor = Predict(x)
-
-
-
-
