26190ecc039381cebef8aef818e0ba6980f7efdb
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2021-03-20 00:50:15 +0300

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -8,11 +8,12 @@ from PIL import Image
 # Images
 img1 = Image.open('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
 
+
 results = model(img1, size=320)
 
 # Inference
 results = model(img1, size=640)  # includes NMS
 results.print()  
 results.save()  # or .show()
-
+st.image("results/BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg")
 # Data
