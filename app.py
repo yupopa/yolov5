d7b39e667a3813a29ad3365e3ffb7f903766bab7
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-20 00:30:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from PIL import Image
 
 x = "best.pt"
 model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model= x)
+model = model.autoshape()
 uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
 
 
