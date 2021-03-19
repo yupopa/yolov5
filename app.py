3764e85c1d7825de7e570de2e1ea56dbd1c6bffe
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-20 00:32:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ class Predict:
     def display_output(self):
         # Inference
         
-        result = model(uploaded_file, size=640)
+        result = model(uploaded_file, size=320)
         results.render()  # updates results.imgs with boxes and labels
         # includes NMS
         result.print()  
