7ba13291cbc3fce8e17bcf1ddb55bcb156cb742b
leibniz35 <79407758+leibniz35@users.noreply.github.com>
2021-03-20 00:43:43 +0300

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,1 +1,18 @@
-print("hello")
+
+import torch
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
+
+from PIL import Image
+
+# Images
+img1 = Image.open('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
+
+results = model(img1, size=320)
+
+# Inference
+results = model(img1, size=640)  # includes NMS
+results.print()  
+results.save()  # or .show()
+
+# Data
