fd18b3d0005876279f1fa52744a52b97958733dd
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-20 12:31:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,52 +2,28 @@ import glob
 import torch
 from urllib.request import urlretrieve
 from PIL import Image
-from IPython.display import Image, display
 import streamlit as st
-from fastai import *
-from fastai.vision.widgets import *
-from fastai.vision.all import *
+
+url = ("http://dl.dropboxusercontent.com/s/fkdy4rbf8g8wm2s/best.pt?raw=1")
+filename = "best.pt"
+urlretrieve(url,filename)
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=filename)
+
+
+uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
 
 
 
-x = "best.pt"
+im_paths = glob.glob('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
 
-im_paths = glob.glob('uploaded_file')
+for i in range(len(im_paths)):
+  img = Image.open(im_paths[i])
+  results = model(img, size=160)  # includes NMS
+  results.print()  
+  results.save()
+from IPython.display import Image, display
 
-
-
-
-class Predict:
-    def __init__(self, filename):
-        model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model= x)
-        self.img = self.get_image_from_upload()
-        if self.img is not None:
-            self.display_output()
- 
-            
+for imageName in glob.glob('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg'): #assuming JPG
+    display(Image(imageName))
     
-    @staticmethod
-    def get_image_from_upload():
-        uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
-        if uploaded_file is not None:
-            return PILImage.create((uploaded_file))
-        return None
-        
-        
-        
-
-    def display_output(self):
-        for i in range(len(im_paths)):
-            img = Image.open(im_paths[i])
-            results = model(img, size=160)  # includes NMS
-            results.print()  
-            results.save()
-
-       
-        for imageName in glob.glob('/results/*.jpg'): 
-            display(Image(filename=imageName))
-            st.image(Image(filename=imageName)
- 
-if __name__=='__main__':
-    predictor = Predict(x)
-
