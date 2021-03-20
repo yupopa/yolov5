3999aa7bd43699c34a9db73b95fbc2f045c7d3fa
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-20 11:34:08 +0300

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,30 +1,18 @@
-import glob
 import torch
-from urllib.request import urlretrieve
-from PIL import Image
-from IPython.display import Image, display
 import streamlit as st
-from fastai import *
-from fastai.vision.widgets import *
-from fastai.vision.all import *
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
+
+from PIL import Image
+
+# Images
 
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model="best.pt")
+results = model(img1, size=320)
 
-
-
-
-uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
-im_paths = glob.glob('uploaded_file')
-
-
-for i in range(len(im_paths)):
-  img = Image.open(im_paths[i])
-  results = model(img, size=160)  # includes NMS
-  results.print()  
-  results.save()
-  
-  
-
-for imageName in glob.glob('uploaded_file'): #assuming JPG
-    st.image(display(Image(filename=imageName)))
+# Inference
+results = model(img1, size=640)  # includes NMS
+results.print()  
+results.save()  # or .show()
+st.image("results/BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg")
+# Data
