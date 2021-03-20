95e3fedfba3536344c07822e76098969de424af1
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-20 11:59:08 +0300

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,20 +1,42 @@
-import torch
+from fastai.vision.widgets import *
+from fastai.vision.all import *
+
+from pathlib import Path
+
 import streamlit as st
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
+class Predict:
+    def __init__(self, filename):
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model= "best.pt")
+        
+        self.img = self.get_image_from_upload()
+        if self.img is not None:
+            self.display_output()
+            self.get_prediction()
+    
+    @staticmethod
+    def get_image_from_upload():
+        uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+        if uploaded_file is not None:
+            return PILImage.create((uploaded_file))
+        return None
 
-from PIL import Image
-uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+    def display_output(self):
+        st.image(self.img.to_thumb(500,500), caption='Uploaded Image')
 
-# Images
-img1 = Image.open(uploaded_file)
+    def get_prediction(self):
+        
 
+        if st.button('Classify'):
+            results = model(img, size=160)  # includes NMS
+            results.print()  
+            results.save()
+            st.write(results.print())
+        else: 
+            st.write(f'Click the button to classify') 
 
-results = model(img1, size=320)
+if __name__=='__main__':
 
-# Inference
-results = model(img1, size=640)  # includes NMS
-results.print()  
-results.save()  # or .show()
-st.image("results/BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg")
-# Data
+    file_name='best.pt'
+
+    predictor = Predict(file_name)
