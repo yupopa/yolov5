da61f5dc25a23f37bf6b2bbc357e255de327d4a0
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-20 15:30:45 +0300

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -14,47 +14,34 @@ urlretrieve(url,filename)
 
 
 
-im_paths = glob.glob('uploaded_file')
-
-
-model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model= filename)
-
 
 class Predict:
     def __init__(self, filename):
-        model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model= filename)
+        self.learn_inference = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model= filename)
         self.img = self.get_image_from_upload()
         if self.img is not None:
             self.display_output()
-            self.get_prediction()
  
             
     
     @staticmethod
     def get_image_from_upload():
         uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+
         if uploaded_file is not None:
-            return img =  Image.open("BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg")
+            return PILImage.create((uploaded_file))
         return None
         
         
         
 
     def display_output(self):
-     
-        for imageName in glob.glob('/results/*.jpg'): 
-            st.image(Image(imageName))
+            self.learn_inference.results = model(self.img, size=160)  # includes NMS
+            self.learn_inference.results.print()  
+            self.learn_inference.results.save()
+            st.image('/results/*.jpg')
             
-
-    def get_prediction(self):
-        if st.button('Classify'):
-            results = model(self.img, size=160)  # includes NMS
-            results.print()  
-            results.save()
-            st.write(results.print())
-        else: 
-            st.write(f'Click the button to classify') 
-
 if __name__=='__main__':
     predictor = Predict(filename)
+    
     
