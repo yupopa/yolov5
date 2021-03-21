af3955d5e373653c488aae16fc583529513d1aee
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-21 17:40:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import torch
 import streamlit as st
 import requests
-
+import glob
+import torch
+from urllib.request import urlretrieve
+from PIL import Image
+import streamlit as st
+import streamlit as st
+import requests
+import base64
+import io
+from PIL import Image
+import glob
+from base64 import decodebytes
+from io import BytesIO
+import numpy as np
+import matplotlib.pyplot as plt
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
 
