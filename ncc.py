8860bd7ab201cc86376308832d13c78d58c841b1
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-22 02:25:13 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -27,6 +27,31 @@ if uploaded_file is not None:
     image = torch.tensor(np.array(image)).permute((2,0,1)).unsqueeze(0)
     image = image.float()/255
     
+    
+    
+    
+    
+    
+    
+    
+from io import BytesIO
+from PIL import Image, ImageFile
+import numpy
+from rawkit import raw
+def convert_cr2_to_jpg(raw_image):
+    raw_image_process = raw.Raw(raw_image)
+    buffered_image = numpy.array(raw_image_process.to_buffer())
+    if raw_image_process.metadata.orientation == 0:
+        jpg_image_height = raw_image_process.metadata.height
+        jpg_image_width = raw_image_process.metadata.width
+    else:
+        jpg_image_height = raw_image_process.metadata.width
+        jpg_image_width = raw_image_process.metadata.height
+    jpg_image = Image.frombytes('RGB', (jpg_image_width, jpg_image_height), buffered_image)
+    return jpg_image
+
+image = convert_cr2_to_jpg(uploaded_file)
+    
 model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=filename)
 with torch.no_grad():
     output=model(image)
