6eb94256962222c80090ea8ed604d684ce5aba6a
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-22 00:06:23 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -19,15 +19,11 @@ urlretrieve(url,filename)
 
 
 uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+if uploaded_file is not None:
+    #try the below line instead of Image.open()
+    image= uploaded_file.read()
 
-if uploaded_file is None:
-    # Default image.
-    url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
-    image = Image.open(requests.get(url, stream=True).raw)
-
-else:
-    # User-selected image.
-    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.')
    
 
 # Convert to JPEG Buffer.
