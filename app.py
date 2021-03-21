430efdc04d5f3d82dad9b876040c22951566ff16
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-21 17:38:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,32 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
 
 from PIL import Image
 
-# Images
-img1 = Image.open('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
+uploaded_file = st.sidebar.file_uploader('',
+                                         type=['png', 'jpg', 'jpeg'],
+                                         accept_multiple_files=False)
+if uploaded_file is None:
+    # Default image.
+    url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
+    image = Image.open(requests.get(url, stream=True).raw)
 
-results = model(img1, size=320)
+else:
+    # User-selected image.
+    image = Image.open(uploaded_file)
+    
+buffered = io.BytesIO()
+image.save(buffered, quality=90, format='JPEG')
+img_str = base64.b64encode(buffered.getvalue())
+img_str = img_str.decode('ascii')   
+
+  
+
+
 
 # Inference
-results = model(img1, size=640)  # includes NMS
+results = model(image, size=640)  # includes NMS
 results.print()  
 results.save()  # or .show()
-st.image("results/BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg")
+st.image("results/image",
+         use_column_width=True)
+
 # Data
