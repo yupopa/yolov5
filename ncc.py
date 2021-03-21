4b62c9efaa36d5c8fca5987927c1c32a2908b1fc
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-21 22:30:12 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -22,13 +22,27 @@ uploaded_file = st.file_uploader('',
 
 if uploaded_file is None:
     # Default image.
-    url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
-    image = Image.open(requests.get(url, stream=True).raw)
+  url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
+    
+  model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model="best.pt")
+
+
+
+  model.results = model(image, size=640)
+  model.results.save()
+
+  image = Image.open(requests.get(url, stream=True).raw)
 
 else:
     # User-selected image.
-    image = Image.open(uploaded_file)
+  image = Image.open(uploaded_file)
+  model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model="best.pt")
 
+
+
+  model.results = model(image, size=640)
+  model.results.save()
+  
 ## Title.
 st.write('# Blood Cell Count Object Detection')
 
