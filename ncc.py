0d570cb01afa11e0a70d36dd49599e01d396cbd3
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-21 18:32:39 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -1,1 +1,40 @@
 
+import streamlit as st
+import requests
+import base64
+import io
+from PIL import Image
+import glob
+from base64 import decodebytes
+from io import BytesIO
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
