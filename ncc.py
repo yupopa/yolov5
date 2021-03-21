5a980813ef06400ea0f8f9bf0493ef94cce92c6d
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-21 18:35:57 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -1,40 +1,18 @@
+import torch
 
-import streamlit as st
-import requests
-import base64
-import io
+model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
+
 from PIL import Image
-import glob
-from base64 import decodebytes
-from io import BytesIO
-import numpy as np
-import matplotlib.pyplot as plt
 
+# Images
+img1 = Image.open('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
 
+results = model(img1, size=320)
 
+# Inference
+results = model(img1, size=640)  # includes NMS
+results.print()  
+results.save()  # or .show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Data
+st.image("results/BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg")
