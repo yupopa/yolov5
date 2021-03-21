a6a9d3e20a8c43df1001fc843f06aa147927360c
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-22 00:41:40 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -51,7 +51,7 @@ st.write('# Blood Cell Count Object Detection')
 model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model="best.pt")
 
 
-model.results = model(image)
+model.results = model(dataBytesIO)
 
 
 
