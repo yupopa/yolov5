0a30e1e6e17a38f2ef620ea85cacd3493dfecd6a
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-21 19:13:21 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -1,18 +1,105 @@
+import streamlit as st
+import requests
+import base64
+import io
+from PIL import Image
+import glob
+from base64 import decodebytes
+from io import BytesIO
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
 
-from PIL import Image
+##########
+##### Set up sidebar.
+##########
 
-# Images
-img1 = Image.open('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
+# Add in location to select image.
 
-results = model(img1, size=320)
+st.sidebar.write('#### Select an image to upload.')
+uploaded_file = st.sidebar.file_uploader('',
+                                         type=['png', 'jpg', 'jpeg'],
+                                         accept_multiple_files=False)
 
-# Inference
-results = model(img1, size=640)  # includes NMS
-results.print()  
-results.save()  # or .show()
+st.sidebar.write('[Find additional images on Roboflow.](https://public.roboflow.com/object-detection/bccd/)')
 
-# Data
-st.image("results/BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg")
+## Add in sliders.
+confidence_threshold = st.sidebar.slider('Confidence threshold: What is the minimum acceptable confidence level for displaying a bounding box?', 0.0, 1.0, 0.5, 0.01)
+overlap_threshold = st.sidebar.slider('Overlap threshold: What is the maximum amount of overlap permitted between visible bounding boxes?', 0.0, 1.0, 0.5, 0.01)
+
+
+image = Image.open('./images/roboflow_logo.png')
+st.sidebar.image(image,
+                 use_column_width=True)
+
+image = Image.open('./images/streamlit_logo.png')
+st.sidebar.image(image,
+                 use_column_width=True)
+
+##########
+##### Set up main app.
+##########
+
+## Title.
+st.write('# Blood Cell Count Object Detection')
+
+## Pull in default image or user-selected image.
+if uploaded_file is None:
+    # Default image.
+    url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
+    image = Image.open(requests.get(url, stream=True).raw)
+
+else:
+    # User-selected image.
+    image = Image.open(uploaded_file)
+
+## Subtitle.
+st.write('### Inferenced Image')
+
+# Convert to JPEG Buffer.
+buffered = io.BytesIO()
+image.save(buffered, quality=90, format='JPEG')
+
+# Base 64 encode.
+img_str = base64.b64encode(buffered.getvalue())
+img_str = img_str.decode('ascii')
+
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model="best.pt")
+
+-----------------------------------------------------
+model.results = model(image, size=640)
+results.save()
+
+image = Image.open(BytesIO(r.content))
+
+# Convert to JPEG Buffer.
+buffered = io.BytesIO()
+image.save(buffered, quality=90, format='JPEG')
+
+# Display image.
+st.image(image,
+         use_column_width=True)
+
+----------------------------------------------------------
+
+output_dict = r.json()
+
+## Generate list of confidences.
+confidences = [box['confidence'] for box in output_dict['predictions']]
+
+## Summary statistics section in main app.
+st.write('### Summary Statistics')
+st.write(f'Number of Bounding Boxes (ignoring overlap thresholds): {len(confidences)}')
+st.write(f'Average Confidence Level of Bounding Boxes: {(np.round(np.mean(confidences),4))}')
+
+## Histogram in main app.
+st.write('### Histogram of Confidence Levels')
+fig, ax = plt.subplots()
+ax.hist(confidences, bins=10, range=(0.0,1.0))
+st.pyplot(fig)
+
+## Display the JSON in main app.
+st.write('### JSON Output')
+st.write(r.json())
