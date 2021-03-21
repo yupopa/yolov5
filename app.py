eed8f0a92ef43a5001ec89aaca11d02d64e59b50
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-21 17:31:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
-import glob
 import torch
-from urllib.request import urlretrieve
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model='best.pt')
+
 from PIL import Image
-import streamlit as st
 
-url = ("http://dl.dropboxusercontent.com/s/fkdy4rbf8g8wm2s/best.pt?raw=1")
-filename = "best.pt"
-urlretrieve(url,filename)
+# Images
+img1 = Image.open('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=filename)
+results = model(img1, size=320)
 
-
-
-
-
-
-im_paths = glob.glob('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg')
-
-for i in range(len(im_paths)):
-  img = Image.open(im_paths[i])
-  results = model(img, size=160)  # includes NMS
-  results.print()  
-  results.save()
-from IPython.display import Image, display
-
-for imageName in glob.glob('BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg'): #assuming JPG
-    display(Image(imageName))
-    
+# Inference
+results = model(img1, size=640)  # includes NMS
+results.print()  
+results.save()  # or .show()
+st.image("results/BloodImage_00002_jpg.rf.72d4182864da81e2fc804f5382965abc.jpg")
+# Data
