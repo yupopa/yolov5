59cef02662cd3cad2fa4de4c9f8698964f3aea19
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-22 13:42:13 +0300

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -6,92 +6,45 @@ from PIL import Image
 import glob
 from base64 import decodebytes
 from io import BytesIO
-import numpy as np
-import torch
+import numpy
 import matplotlib.pyplot as plt
+import torch
 from urllib.request import urlretrieve
+from fastai.vision.widgets import *
+from fastai.vision.all import *
+import cv2
 
-##########
-##### Set up sidebar.
-##########
 
-# Add in location to select image.
+
 url = ("http://dl.dropboxusercontent.com/s/fkdy4rbf8g8wm2s/best.pt?raw=1")
 filename = "best.pt"
 urlretrieve(url,filename)
 
-st.sidebar.write('#### Select an image to upload.')
-uploaded_file = st.sidebar.file_uploader('',
-                                         type=['png', 'jpg', 'jpeg'],
-                                         accept_multiple_files=False)
-
-st.sidebar.write('[Find additional images on Roboflow.](https://public.roboflow.com/object-detection/bccd/)')
-
-## Add in sliders.
-confidence_threshold = st.sidebar.slider('Confidence threshold: What is the minimum acceptable confidence level for displaying a bounding box?', 0.0, 1.0, 0.5, 0.01)
-overlap_threshold = st.sidebar.slider('Overlap threshold: What is the maximum amount of overlap permitted between visible bounding boxes?', 0.0, 1.0, 0.5, 0.01)
-
-
-##########
-##### Set up main app.
-##########
-
-## Title.
+urll = ("http://dl.dropboxusercontent.com/s/ecl4tj6q2u8s4q3/fig-03_5.png?raw=1")
+filenamee = "fig-03_5.png"
+urlretrieve(urll,filenamee)
+st.image(filenamee)
 st.write('# Blood Cell Count Object Detection')
 
-## Pull in default image or user-selected image.
+
+uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+
+
+
 if uploaded_file is None:
     # Default image.
     url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
     image = Image.open(requests.get(url, stream=True).raw)
 
 else:
-    # User-selected image.
     image = Image.open(uploaded_file)
-
-## Subtitle.
-st.write('### Inferenced Image')
-
-# Convert to JPEG Buffer.
-buffered = io.BytesIO()
-image.save(buffered, quality=90, format='JPEG')
-
-# Base 64 encode.
-img_str = base64.b64encode(buffered.getvalue())
-img_str = img_str.decode('ascii')
-
-model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model="best.pt")
-model.results = model(image)
+    img_array = np.array(image)
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=filename)
+    model.results = model(img_array, size=640)
+    model.results.save()  # or .show()
+    st.image("results/image0.jpg")
+    
 
 
-image = Image.open(BytesIO(r.content))
+    
 
-# Convert to JPEG Buffer.
-buffered = io.BytesIO()
-image.save(buffered, quality=90, format='JPEG')
-
-# Display image.
-st.image(image,
-         use_column_width=True)
-
-
-
-
-
-## Generate list of confidences.
-confidences = [box['confidence'] for box in output_dict['predictions']]
-
-## Summary statistics section in main app.
-st.write('### Summary Statistics')
-st.write(f'Number of Bounding Boxes (ignoring overlap thresholds): {len(confidences)}')
-st.write(f'Average Confidence Level of Bounding Boxes: {(np.round(np.mean(confidences),4))}')
-
-## Histogram in main app.
-st.write('### Histogram of Confidence Levels')
-fig, ax = plt.subplots()
-ax.hist(confidences, bins=10, range=(0.0,1.0))
-st.pyplot(fig)
-
-## Display the JSON in main app.
-st.write('### JSON Output')
-st.write(r.json())
