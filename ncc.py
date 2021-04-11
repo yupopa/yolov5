2e526b1d2586cce3570e51d3a24f3b0fbe25460b
yupopa <61375250+yupopa@users.noreply.github.com>
2021-04-11 15:26:20 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -46,7 +46,7 @@ else:
     image = Image.open(uploaded_file)
     img_array = np.array(image).device("cpu")
     
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=filename).device("cpu")
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=filename,map_location=torch.device('cpu'))
     model.results = model(img_array, size=640)
     model.results.save()  # or .show()
     st.image("results/image0.jpg")
