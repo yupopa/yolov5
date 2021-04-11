bf4a7fba9daaba8de838f894503664b26907c6b3
yupopa <61375250+yupopa@users.noreply.github.com>
2021-04-11 15:49:05 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -45,7 +45,7 @@ else:
     image = Image.open(uploaded_file)
     img_array = np.array(image)
 
-    
+    torch.load('best.pt',map_location='cpu')
     model = torch.hub.load("ultralytics/yolov5", "custom", "best.pt", force_reload=True)
     
     model.results = model(img_array, size=640)
