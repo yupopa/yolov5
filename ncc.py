f0d6e4b19c97197799ff8c978d8c09fcc5f2c86d
yupopa <61375250+yupopa@users.noreply.github.com>
2021-04-11 15:55:08 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -44,10 +44,9 @@ if uploaded_file is None:
 else:
     image = Image.open(uploaded_file)
     img_array = np.array(image)
-    model = torch.load('best.pt', map_location=lambda storage, loc: storage.cuda(1))
-    
-    model = torch.load('best.pt', map_location={'cuda:1':'cuda:0'})
     model = torch.hub.load("ultralytics/yolov5", "custom", "best.pt")
+    model = torch.load('best.pt', map_location=torch.device('cpu'))
+
    
     
     model.results = model(img_array, size=640)
