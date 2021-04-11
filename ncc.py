04248e534745a8c990dd4a2fad2b1a13667bef17
yupopa <61375250+yupopa@users.noreply.github.com>
2021-04-11 16:16:27 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -50,7 +50,7 @@ else:
     
     model.results = model(img_array, size=640)
     model.results.save()  # or .show()
-    st.image("runs/hub/exp/image1.jpg")
+    st.image("runs/hub/exp/image0.jpg")
   
     liste = []
     liste1 = []
