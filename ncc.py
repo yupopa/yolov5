516364a6b967bfa156823722d2756340a1388950
yupopa <61375250+yupopa@users.noreply.github.com>
2021-04-11 15:23:08 +0300

    Update ncc.py

--- ncc.py
+++ ncc.py
@@ -44,9 +44,9 @@ if uploaded_file is None:
 
 else:
     image = Image.open(uploaded_file)
-    img_array = np.array(image)
+    img_array = np.array(image).device("cpu")
     
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=filename)
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=filename).device("cpu")
     model.results = model(img_array, size=640)
     model.results.save()  # or .show()
     st.image("results/image0.jpg")
